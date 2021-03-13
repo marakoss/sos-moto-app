98a7626f2356795d0d43a6b5201975093158ecc5
Marek Králík <marakoss@gmail.com>
2021-03-14 00:44:44 +0100

    Introduced icons

---
+++ src/assets/images/icons/icon-arrow.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Svg, { Polygon } from 'react-native-svg';
+import { mySvgIcon } from 'types/svg';
+
+const IconArrow: mySvgIcon = ({
+  fillColor,
+  fillColor2,
+  width,
+  height
+}): JSX.Element => {
+
+  const myWidth = width ? width : 50;
+  const myHeight = height ? height : 50;
+
+  return (
+    <Svg
+      width={myWidth}
+      height={myHeight}
+      viewBox="0 0 50 50"
+    >
+      <Polygon fill={fillColor?fillColor:'#000'} points="25,35 45,49 25,0 5,49 "/>
+      <Polygon fill={fillColor2?fillColor2:'#f00'} points="25,35 45,49 25,0 "/>
+    </Svg>
+  )
+};
+
+export default IconArrow;
---
+++ src/assets/images/icons/icon-blob.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import Svg, { Path } from 'react-native-svg';
+import { mySvgIcon } from 'types/svg';
+
+const IconBlob: mySvgIcon = ({
+  width,
+  height,
+  fillColor
+}): JSX.Element => {
+  return (
+    <Svg
+      width={Number(width) | 80}
+      height={Number(height) | 80}
+      viewBox="0 0 120 120"
+      fill={String(fillColor ? fillColor : "#000000")}
+    >  
+      <Path d="M 108.833 7.593 C 98.709 -2.531 17.717 -2.531 7.593 7.593 C -2.531 17.717 -2.531 98.709 7.593 108.833 C 17.717 118.957 98.709 118.957 108.833 108.833 C 118.957 98.709 118.957 17.717 108.833 7.593 Z"/>
+    </Svg>
+  )
+};
+
+export default IconBlob;
+
+
+
