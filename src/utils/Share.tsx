ec24728fa5cb3747d92096ef70b82650d110b467
Marek Králík <marakoss@gmail.com>
2021-04-02 18:33:31 +0200

    Updated share button

--- src/utils/Share.tsx
+++ src/utils/Share.tsx
@@ -1,16 +1,22 @@
 import { Share } from 'react-native';
 import { toDecimalPlaces } from '@utils/Math';
+import * as Location from 'expo-location';
 
-export const onShare = async (lat: number, lon: number) => {
-	try {
-		const result = await Share.share({
-			message: `Mám potíže na cestě. Moje poloha je: ${toDecimalPlaces(
-				lat,
-				5
-			).toFixed(5)}, ${toDecimalPlaces(lon, 5).toFixed(5)}`
+export const onShare = (lat: number, lon: number) => {
+	const formatedLat = toDecimalPlaces(lat, 5).toFixed(5);
+	const formatedLon = toDecimalPlaces(lon, 5).toFixed(5);
+
+	(async () => {
+		const geoCode = await Location.reverseGeocodeAsync({
+			latitude: lat,
+			longitude: lon
 		});
+
+		const message = `Mám potíže na cestě. Nacházím se blízko místa: ${geoCode[0].city}. Moje GPS souřadnice jsou: ${formatedLat}, ${formatedLon}. https://www.google.com/maps/dir/?api=1&travelmode=driving&destination=${lat},${lon}`;
+
+		const result = await Share.share({ message: message });
 		return result;
-	} catch (error) {
-		// nothing to do here
-	}
+	})().then(() => {
+		// Success
+	});
 };
