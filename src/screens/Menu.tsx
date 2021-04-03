9698fbb51dcdcd3229dc0fa433e3b999105dbb8b
Marek Králík <marakoss@gmail.com>
2021-04-03 04:23:20 +0200

    Updated Menu page

--- src/screens/Menu.tsx
+++ src/screens/Menu.tsx
@@ -1,59 +1,131 @@
 import React, { FC } from 'react';
-import { StyleSheet, View, SafeAreaView } from 'react-native';
+import {
+	StyleSheet,
+	View,
+	ScrollView,
+	SafeAreaView,
+	Text,
+	Image
+} from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
 import i18n from 'i18n-js';
-import { Headline, Button } from '@components/index';
-import { IconNavigate } from '@icons/index';
+import { Headline, Button, Background } from '@components/index';
+import { IconNavigate, IconPerson, IconInfo } from '@icons/index';
+import { IMAGEABOUT } from '@images/banners';
 import globalStyle from '@components/Platform/globalStyle';
+import { LinearGradient } from 'expo-linear-gradient';
 import { COLORS } from '@dictionaries/colors';
 
 const Menu: FC<StackScreenProps<any>> = ({ navigation }) => {
 	return (
 		<View style={s.container}>
+			<View style={s.headerContainer}>
+				<LinearGradient
+					colors={[
+						COLORS.BACKGROUNDGRADIENT1,
+						COLORS.BACKGROUNDGRADIENT2
+					]}
+					start={{ x: 0, y: 0 }}
+					end={{ x: 0, y: 1.5 }}
+				>
+					<Image source={IMAGEABOUT} style={s.bannerImage} />
+					<View style={s.header}>
+						<Headline
+							headline={i18n.t('Navigate')}
+							textColor={{ color: COLORS.WHITE }}
+						/>
+					</View>
+				</LinearGradient>
+			</View>
 			<SafeAreaView style={s.safeArea}>
-				<View style={s.header}>
-					<Headline
-						headline={i18n.t('Navigate')}
-						textColor={{ color: COLORS.BLACK }}
-					/>
-				</View>
-
-				<View style={s.content}>
-					<Button
-						onPress={() => navigation.navigate('Home')}
-						icon={<IconNavigate />}
+				<Background>
+					<ScrollView
+						style={s.content}
+						showsVerticalScrollIndicator={false}
 					>
-						{i18n.t('back to')}
-						{' '}
-						{i18n.t('Radar')}
-					</Button>
+						<Button
+							onPress={() => navigation.navigate('Home')}
+							icon={() => IconNavigate}
+							iconFillColor={COLORS.DARKERTEXT}
+							iconHoverFillColor={COLORS.WHITE}
+						>
+							{i18n.t('back to')} {i18n.t('Radar')}
+						</Button>
 
-					<Button onPress={() => navigation.navigate('Register')}>
-						{i18n.t('Register')}
-					</Button>
+						<View style={[s.item, { borderColor: COLORS.PRIMARY }]}>
+							<Text style={s.menuItemDescription}>
+								{i18n.t('menu_registerText')}
+							</Text>
+							<Button
+								onPress={() => navigation.navigate('Register')}
+								icon={() => IconPerson}
+								iconFillColor={COLORS.DARKERTEXT}
+								iconHoverFillColor={COLORS.WHITE}
+							>
+								{i18n.t('Register')}
+							</Button>
+						</View>
 
-					<Button onPress={() => navigation.navigate('About')}>
-						{i18n.t('About')}
-					</Button>
-				</View>
+						<View
+							style={[
+								s.item,
+								{ borderColor: COLORS.BACKGROUNDGRADIENT2 }
+							]}
+						>
+							<Text style={s.menuItemDescription}>
+								{i18n.t('menu_aboutText')}
+							</Text>
+							<Button
+								onPress={() => navigation.navigate('About')}
+								icon={() => IconInfo}
+								iconFillColor={COLORS.DARKERTEXT}
+								iconHoverFillColor={COLORS.WHITE}
+							>
+								{i18n.t('About')}
+							</Button>
+						</View>
+					</ScrollView>
+				</Background>
 			</SafeAreaView>
 		</View>
 	);
 };
 
 const s = StyleSheet.create({
-	header: {
-		paddingBottom: 20
-	},
-	content: {
-		paddingHorizontal: 20
-	},
 	container: {
 		backgroundColor: COLORS.WHITE,
 		flex: 1
 	},
 	safeArea: {
-		...globalStyle.droidSafeArea
+		...globalStyle.droidSafeArea,
+		flex: 1,
+		marginTop: -20
+	},
+	headerContainer: {
+		position: 'relative'
+	},
+	header: {
+		position: 'absolute',
+		top: 85,
+		right: 20
+	},
+	content: {
+		paddingHorizontal: 20
+	},
+	bannerImage: {
+		width: '100%',
+		height: 200,
+		maxHeight: 200
+	},
+	menuItemDescription: {
+		paddingBottom: 10,
+		color: COLORS.BLACK
+	},
+	item: {
+		borderLeftWidth: 2,
+		paddingHorizontal: 20,
+		paddingVertical: 10,
+		marginTop: 20
 	}
 });
 
