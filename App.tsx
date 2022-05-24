d04a0784da27f42822d5b32ca29054669d3ed2df
Marek Králík <marakoss@gmail.com>
2022-05-24 02:18:16 +0200

    Created a new Expo app

--- App.tsx
+++ App.tsx
@@ -1,91 +1,20 @@
-import 'react-native-gesture-handler'; // Must be at top for some reason
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-import React, { FC, useEffect, useReducer } from 'react';
-
-import { activateKeepAwake } from 'expo-keep-awake';
-
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-import { Main, Filter, Menu, Register, About } from '@screens/index';
-
-import {
-	FiltersContext,
-	filtersReducer,
-	filters,
-	MobileContext,
-	mobileReducer,
-	mobile,
-	LocationContext,
-	locationReducer,
-	location
-} from '@store/index';
-
-import i18n from 'i18n-js';
-import * as Localization from 'expo-localization';
-import { cs, en, de, sk, pl } from '@translations/index';
-
-if (__DEV__) {
-	console.log('Language was set to: ', Localization.locale);
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
-i18n.translations = {
-	en: en,
-	cs: cs,
-	sk: sk,
-	de: de,
-	pl: pl
-};
-i18n.locale = Localization.locale;
-i18n.defaultLocale = 'en';
-i18n.fallbacks = true;
-
-const Stack = createStackNavigator();
-
-const App: FC = () => {
-	// TODO: Rework as HOC?
-	const [filterState, dispatchFilter] = useReducer(filtersReducer, filters);
-	const [mobileState, dispatchMobile] = useReducer(mobileReducer, mobile);
-	const [locationState, dispatchLocation] = useReducer(
-		locationReducer,
-		location
-	);
-
-	useEffect(() => {
-		if (__DEV__) {
-			activateKeepAwake();
-		}
-	}, []);
-
-	return (
-		<MobileContext.Provider
-			value={{ ...mobileState, dispatch: dispatchMobile }}
-		>
-			<LocationContext.Provider
-				value={{ ...locationState, dispatch: dispatchLocation }}
-			>
-				<FiltersContext.Provider
-					value={{ ...filterState, dispatch: dispatchFilter }}
-				>
-					<NavigationContainer>
-						<Stack.Navigator
-							initialRouteName="Home"
-							headerMode="none"
-						>
-							<Stack.Screen name="Home" component={Main} />
-							<Stack.Screen name="Filter" component={Filter} />
-							<Stack.Screen name="Menu" component={Menu} />
-							<Stack.Screen
-								name="Register"
-								component={Register}
-							/>
-							<Stack.Screen name="About" component={About} />
-						</Stack.Navigator>
-					</NavigationContainer>
-				</FiltersContext.Provider>
-			</LocationContext.Provider>
-		</MobileContext.Provider>
-	);
-};
-
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
